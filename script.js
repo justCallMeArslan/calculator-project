09e02eb090aacee9cdb35678730bc534f114bf8e
arslan <244682724+justCallMeArslan@users.noreply.github.com>
2026-01-25 17:34:22 +0300

    Complete basic calculating logic.

---
+++ script.js
@@ -0,0 +1,37 @@
+let aNumber;
+let bNumber;
+let operator;
+
+function add(a, b){
+    return a + b;
+};
+
+function substract(a, b) {
+    return a - b;
+};
+
+function multiply(a, b) {
+    return a * b;
+};
+
+function divide(a, b) {
+    return a / b;
+}; 
+
+function operate(aNumber, operator, bNumber){
+    switch (operator) {
+        case "+":
+            return add(aNumber, bNumber);
+        case "-":
+            return substract(aNumber, bNumber);
+        case "x":
+            return multiply(aNumber, bNumber);
+        case "÷":
+            return divide(aNumber, bNumber);
+        default:
+            return "NaO";
+    }
+
+}
+
+console.log(operate(10, "x", 20));
